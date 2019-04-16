b6ceb75945263dfbc5fc4b4695bea715ca9f1504
aaron <georges-aaron.randrianaina@inria.fr>
2019-04-16 13:02:38 +0200

    feat(launch): more specification in the types linked with the data structures

--- launch.sh
+++ launch.sh
@@ -1,18 +1,28 @@
 for ((size = 1000000000; size <= 1000000000000; size += 10000)); do
     for datastructure in L S D A T; do
-	for t in o i f s; do
-	    if [ "$datastructure" == "D" ] || [ "$datastructure" == "S" ];
-	    then
-		for l in c r ; do
+	if [ "$datastructure" == "S" ];
+	then
+	    for t in i s f; do
+	        for l in l c r w; do
 		    sleep 120
-		    ./tester.sh -n "-$datastructure$t$l" aaronspirals/dstest "-$datastructure$t$l" $size
+		    ./tester.sh -n "$datastructure$t$l$size" aaronspirals/dstest "-$datastructure$t$l" $size
 		done
-	    else
-		for l in l c r w; do
-		    sleep 120
-		    ./tester.sh -n  "-$datastructure$t$l" aaronspirals/dstest "-$datastructure$t$l" $size
-		done
-	    fi
-	done
+	    done
+	else
+	    for t in o i f s; do
+		if [ "$datastructure" == "D" ] || [ "$datastructure" == "S" ];
+		then
+		    for l in c r ; do
+			sleep 120
+			./tester.sh -n "$datastructure$t$l$size" aaronspirals/dstest "-$datastructure$t$l" $size
+		    done
+		else
+		    for l in l c r w; do
+			sleep 120
+		        ./tester.sh -n "$datastructure$t$l$size" aaronspirals/dstest "-$datastructure$t$l" $size
+		    done
+		fi
+	    done
+	fi
     done
 done
