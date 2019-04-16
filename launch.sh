cb18a38abe068edcef61dc4539e7955fd9b85bc8
aaron <georges-aaron.randrianaina@inria.fr>
2019-04-16 16:31:32 +0200

    refactot(launch)

--- launch.sh
+++ launch.sh
@@ -1,25 +1,37 @@
-for ((size = 1000000000; size <= 1000000000000; size += 10000)); do
+sleeptime=1
+startsize=1000000000
+endsize=1000000000000
+nbtests=20
+step=10000
+for ((size = $startsize; size <= $endsize; size += $step)); do
     for datastructure in L S D A T; do
 	if [ "$datastructure" == "S" ];
 	then
 	    for t in i s f; do
-	        for l in l c r w; do
-		    sleep 120
-		    ./tester.sh -n "$datastructure$t$l$size" aaronspirals/dstest "-$datastructure$t$l" $size
+	        for b in l c r w; do
+		    for i in {1..$nbtests}; do
+			sleep $sleeptime
+			./tester.sh -n "$datastructure$t$b_$size" aaronspirals/dstest "-$datastructure$t$b" $size
+		    done
 		done
 	    done
 	else
 	    for t in o i f s; do
 		if [ "$datastructure" == "D" ] || [ "$datastructure" == "S" ];
 		then
-		    for l in c r ; do
-			sleep 120
-			./tester.sh -n "$datastructure$t$l$size" aaronspirals/dstest "-$datastructure$t$l" $size
+		    for b in c r ; do
+			for i in {1..$nbtests}; do
+			    sleep $sleeptime
+			    ./tester.sh -n "$datastructure$t$b_$size" aaronspirals/dstest "-$datastructure$t$b" $size
+			done
+			
 		    done
 		else
-		    for l in l c r w; do
-			sleep 120
-		        ./tester.sh -n "$datastructure$t$l$size" aaronspirals/dstest "-$datastructure$t$l" $size
+		    for b in l c r w; do
+			for i in {1..$nbtests}; do
+			    sleep $sleeptime
+			    ./tester.sh -n "$datastructure$t$b_$size" aaronspirals/dstest "-$datastructure$t$b" $size
+			done
 		    done
 		fi
 	    done
