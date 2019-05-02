f0b74d11ca5f8509a420d899ba67dc95ce0ac55e
aaron <georges-aaron.randrianaina@inria.fr>
2019-05-02 17:38:17 +0200

    feat(launch): add cases for insertion

--- data_structures/launch.sh
+++ data_structures/launch.sh
@@ -1,7 +1,7 @@
-startsize=10000
+startsize=100000
 endsize=1000000
 step=10000
-docker_image=aaronspirals/pythonDS
+docker_image=aaronspirals/pythonds
 
 opt_list=('iteration-for' 'iteration-while' 'iteration-for-range' 'iteration-comp' 'insertion-beginning' 'insertion-middle' 'inertion-end' 'random-access' 'random-removal' 'clean' 'pop' 'extend');
 
@@ -13,31 +13,83 @@ types=('integer' 'float' 'str');
 
 data_structures=('list' 'set' 'dict');
 
-
 for ((size=$startsize; size <= $endsize; size += $step)); do
     for ds in "${data_structures[@]}"; do
 	for t in "${types[@]}"; do
 	    if [ "$ds" == "list" ]; then
 		for opt in "${opt_list[@]}"; do
-		    for i in {1..20}; do
-			./tester.sh -n "$ds"_"$opt"_"$size" "$docker_image" --action "$opt" --size $size --data-structure "$ds" --type "$t"
-			# echo python main.py --action "$opt" --size $size --data-structure "$ds" --type "$t"
-			# python main.py --action "$opt" --size $size --data-structure "$ds" --type "$t"
-		    done
+		    # cas d'insertion dans la liste
+		    if [[ "$opt" =~ "insertion" ]]; then
+			for d in {1..3}; do
+			    # Pour ajouter tous les éléments, la moitié puis
+			    # le tiers
+			    for i in {1..20}; do
+				# nombre d'éléments à insérer
+				ins=$(( $size / $d ))
+				sync; echo 3 > /proc/sys/vm/drop_caches
+				swapoff -a && swapon -a
+				./tester.sh -n "$ds"_"$opt"_"$ins"_"$size" "$docker_image" --action "$opt" --size $size --data-structure "$ds" --type "$t" --extra $ins
+				# echo python main.py --action "$opt" --size $size --data-structure "$ds" --type "$t"
+				# python main.py --action "$opt" --size $size --data-structure "$ds" --type "$t"
+			    done
+			done
+		    else	# sinon
+			for i in {1..20}; do
+			    sync; echo 3 > /proc/sys/vm/drop_caches
+			    swapoff -a && swapon -a
+			    ./tester.sh -n "$ds"_"$opt"_"$size" "$docker_image" --action "$opt" --size $size --data-structure "$ds" --type "$t"
+			done
+		    fi
 		done
 	    fi
 	    if [ "$ds" == "dict" ]; then
 		for opt in "${opt_dict[@]}"; do
-		    for i in {1..20}; do
-			./tester.sh -n "$ds"_"$opt"_"$size" "$docker_image" --action "$opt" --size $size --data-structure "$ds" --type "$t"
-		    done
+		    if [[ "$opt" =~ "insertion" ]]; then
+			for d in {1..3}; do
+			    # Pour ajouter tous les éléments, la moitié puis
+			    # le tiers
+			    for i in {1..20}; do
+				# nombre d'éléments à insérer
+				ins=$(( $size / $d ))
+				sync; echo 3 > /proc/sys/vm/drop_caches
+				swapoff -a && swapon -a
+				./tester.sh -n "$ds"_"$opt"_"$ins"_"$size" "$docker_image" --action "$opt" --size $size --data-structure "$ds" --type "$t" --extra $ins
+				# echo python main.py --action "$opt" --size $size --data-structure "$ds" --type "$t"
+				# python main.py --action "$opt" --size $size --data-structure "$ds" --type "$t"
+			    done
+			done
+		    else	# sinon
+			for i in {1..20}; do
+			    sync; echo 3 > /proc/sys/vm/drop_caches
+			    swapoff -a && swapon -a
+			    ./tester.sh -n "$ds"_"$opt"_"$size" "$docker_image" --action "$opt" --size $size --data-structure "$ds" --type "$t"
+			done
+		    fi
 		done
 	    fi
 	    if [ "$ds" == "set" ]; then
 		for opt in "${opt_set[@]}"; do
-		    for i in {1..20}; do
-			./tester.sh -n "$ds"_"$opt"_"$size" "$docker_image" --action "$opt" --size $size --data-structure "$ds" --type "$t"
-		    done
+		    if [[ "$opt" =~ "insertion" ]]; then
+			for d in {1..3}; do
+			    # Pour ajouter tous les éléments, la moitié puis
+			    # le tiers
+			    for i in {1..20}; do
+				# nombre d'éléments à insérer
+				ins=$(( $size / $d ))
+				sync; echo 3 > /proc/sys/vm/drop_caches
+				swapoff -a && swapon -a
+				./tester.sh -n "$ds"_"$opt"_"$ins"_"$size" "$docker_image" --action "$opt" --size $size --data-structure "$ds" --type "$t" --extra $ins
+				# echo python main.py --action "$opt" --size $size --data-structure "$ds" --type "$t"
+				# python main.py --action "$opt" --size $size --data-structure "$ds" --type "$t"
+			    done
+			done
+		    else	# sinon
+			for i in {1..20}; do
+			    sync; echo 3 > /proc/sys/vm/drop_caches
+			    swapoff -a && swapon -a
+			    ./tester.sh -n "$ds"_"$opt"_"$size" "$docker_image" --action "$opt" --size $size --data-structure "$ds" --type "$t"
+			done
+		    fi
 		done
 	    fi
 	done
