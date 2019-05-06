59e370d866d7018a8a635856001f4225ed1ffa45
aaron <georges-aaron.randrianaina@inria.fr>
2019-05-06 16:16:21 +0200

    typo in opt_list

--- data_structures/launch.sh
+++ data_structures/launch.sh
@@ -3,7 +3,7 @@ endsize=1000000
 step=10000
 docker_image=aaronspirals/pythonds
 
-opt_list=('iteration-for' 'iteration-while' 'iteration-for-range' 'iteration-comp' 'insertion-beginning' 'insertion-middle' 'insertion-end' 'random-access' 'random-removal' 'clean' 'pop' 'extend');
+opt_list=('iteration-for' 'iteration-while' 'iteration-for-range' 'insertion-comp' 'insertion-beginning' 'insertion-middle' 'insertion-end' 'random-access' 'random-removal' 'clean' 'pop' 'extend');
 
 opt_dict=('insertion' 'insertion-comp' 'iteration-key' 'iteration-kv' 'not-in' 'random-access');
 
@@ -19,7 +19,7 @@ for ((size=$startsize; size <= $endsize; size += $step)); do
 	    if [ "$ds" == "list" ]; then
 		for opt in "${opt_list[@]}"; do
 		    # cas d'insertion dans la liste
-		    if [[ "$opt" =~ "insertion" ]] || [[ "$opt" == "extend" ]] || [[ "$opt" == "pop" ]]; then
+		    if [[ "$opt" =~ "insertion" ]] || [[ "$opt" == "extend" ]] || [[ "$opt" == "pop" ]] || [[ "$opt" =~ "random" ]] || [[ "$opt" == "" ]] ; then
 			for d in {1..3}; do
 			    # Pour ajouter tous les éléments, la moitié puis
 			    # le tiers
@@ -28,7 +28,7 @@ for ((size=$startsize; size <= $endsize; size += $step)); do
 				ins=$(( $size / $d ))
 				sync;  echo 3 > /proc/sys/vm/drop_caches
 				swapoff -a && swapon -a
-				./tester.sh -n "$ds$t"_"$opt"_"1/$d"_"$size" "$docker_image" --action "$opt" --size $size --data-structure "$ds" --type "$t" --extra $ins
+				./tester.sh -n "$ds$t"_"$opt"_"1-$d"_"$size" "$docker_image" --action "$opt" --size $size --data-structure "$ds" --type "$t" --extra $ins
 				#  python main.py --action "$opt" --size $size --data-structure "$ds" --type "$t"
 				# python main.py --action "$opt" --size $size --data-structure "$ds" --type "$t"
 			    done
@@ -44,7 +44,7 @@ for ((size=$startsize; size <= $endsize; size += $step)); do
 	    fi
 	    if [ "$ds" == "dict" ]; then
 		for opt in "${opt_dict[@]}"; do
-		    if [[ "$opt" =~ "insertion" ]]; then
+		    if [[ "$opt" =~ "insertion" ]] || [[ "$opt" =~ "random" ]] || [[ "$opt" == "not-in" ]] ; then
 			for d in {1..3}; do
 			    # Pour ajouter tous les éléments, la moitié puis
 			    # le tiers
@@ -53,7 +53,7 @@ for ((size=$startsize; size <= $endsize; size += $step)); do
 				ins=$(( $size / $d ))
 				sync;  echo 3 > /proc/sys/vm/drop_caches
 				swapoff -a && swapon -a
-				./tester.sh -n "$ds$t"_"$opt"_"1/$d"_"$size" "$docker_image" --action "$opt" --size $size --data-structure "$ds" --type "$t" --extra $ins
+				./tester.sh -n "$ds$t"_"$opt"_"1-$d"_"$size" "$docker_image" --action "$opt" --size $size --data-structure "$ds" --type "$t" --extra $ins
 				#  python main.py --action "$opt" --size $size --data-structure "$ds" --type "$t"
 				# python main.py --action "$opt" --size $size --data-structure "$ds" --type "$t"
 			    done
@@ -69,7 +69,7 @@ for ((size=$startsize; size <= $endsize; size += $step)); do
 	    fi
 	    if [ "$ds" == "set" ]; then
 		for opt in "${opt_set[@]}"; do
-		    if [[ "$opt" =~ "insertion" ]]; then
+		    if [[ "$opt" =~ "insertion" ]] || [[ "$opt" =~ "random" ]] || [[ "$opt" == "not-in" ]] ; then
 			for d in {1..3}; do
 			    # Pour ajouter tous les éléments, la moitié puis
 			    # le tiers
@@ -78,7 +78,7 @@ for ((size=$startsize; size <= $endsize; size += $step)); do
 				ins=$(( $size / $d ))
 				sync;  echo 3 > /proc/sys/vm/drop_caches
 				swapoff -a && swapon -a
-				./tester.sh -n "$ds"_"$opt"_"1/$d"_"$size" "$docker_image" --action "$opt" --size $size --data-structure "$ds" --type "$t" --extra $ins
+				./tester.sh -n "$ds"_"$opt"_"1-$d"_"$size" "$docker_image" --action "$opt" --size $size --data-structure "$ds" --type "$t" --extra $ins
 				#  python main.py --action "$opt" --size $size --data-structure "$ds" --type "$t"
 				# python main.py --action "$opt" --size $size --data-structure "$ds" --type "$t"
 			    done
